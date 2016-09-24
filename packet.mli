bc6fb29af5c08c28bab4a174e1727e0f2c166a8e
John Kilburg <john@greenjeans.com>
2016-09-24 00:46:30 -0400

    Use _stub instead of _intf for C stub files.
    Add mli files.

---
+++ packet.mli
@@ -0,0 +1,7 @@
+open Core.Std
+
+val socket : int -> (int,int) Result.t
+val bind   : int -> int -> int -> (unit,int) Result.t
+val add_membership  : int -> int -> string -> (unit,int) Result.t
+val drop_membership : int -> int -> string -> (unit,int) Result.t
+val origdev         : int -> int -> (unit,int) Result.t
